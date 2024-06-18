eb55b3737ee022583e3ad7ac17168bd7a11d9804
evgeni1494 <yoni1@hotmail.fr>
2024-06-18 11:35:19 +0200

    MAJ ci-cd pour les tests de la data-pre

--- tests/test_data_preparation.py
+++ tests/test_data_preparation.py
@@ -5,8 +5,3 @@ def test_load_data():
     """Teste si les données sont chargées correctement."""
     data = load_data('../archive/emissions.csv')
     assert not data.empty, "Data should not be empty"
-
-
-
-
-

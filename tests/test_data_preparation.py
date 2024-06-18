46eab0fca8a94c37284b05490a2a153f2e4bd61e
evgeni1494 <yoni1@hotmail.fr>
2024-06-18 11:39:43 +0200

    MAJ path pour emission.csv

--- tests/test_data_preparation.py
+++ tests/test_data_preparation.py
@@ -1,7 +1,10 @@
 import pytest
+import os
 from model.data_preparation import load_data, prepare_data
 
 def test_load_data():
     """Teste si les données sont chargées correctement."""
-    data = load_data('../archive/emissions.csv')
+    current_dir = os.path.dirname(os.path.abspath(__file__))
+    data_path = os.path.join(current_dir, '../archive/emissions.csv')
+    data = load_data(data_path)
     assert not data.empty, "Data should not be empty"

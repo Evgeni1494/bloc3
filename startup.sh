6051ecd5522e49773a0aa095b8657864008abf04
evgeni1494 <yoni1@hotmail.fr>
2024-08-12 12:21:34 +0200

    changement startup

--- startup.sh
+++ startup.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
+
 echo "Installing dependencies"
 pip install -r requirements.txt
 echo "Starting application"
-gunicorn -k uvicorn.workers.UvicornWorker api.main:app --bind 0.0.0.0:$PORT
+gunicorn -k uvicorn.workers.UvicornWorker api.main:app --bind 0.0.0.0:8000

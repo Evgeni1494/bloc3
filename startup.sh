f85339ccf80018ed510d759bcf520c59cffcae75
evgeni1494 <yoni1@hotmail.fr>
2024-10-07 14:56:05 +0200

    .sh modifs

--- startup.sh
+++ startup.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 echo "Installing dependencies"
-PORT=${PORT:-8000}
-
 pip install -r requirements.txt
-echo "Starting application"
-gunicorn -k uvicorn.workers.UvicornWorker api.main:app --bind 0.0.0.0:8000
+
+# Utilisation de la variable d'environnement PORT fournie par Azure
+echo "Starting application on port $PORT"
+gunicorn -k uvicorn.workers.UvicornWorker api.main:app --bind 0.0.0.0:$PORT

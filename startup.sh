33088344a133e8c62a3259982620bfeea162b0d7
evgeni1494 <yoni1@hotmail.fr>
2024-10-07 14:50:55 +0200

    Ajustement startup.sh

--- startup.sh
+++ startup.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
 echo "Installing dependencies"
+PORT=${PORT:-8000}
+
 pip install -r requirements.txt
 echo "Starting application"
 gunicorn -k uvicorn.workers.UvicornWorker api.main:app --bind 0.0.0.0:8000
